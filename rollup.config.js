a1059e115b327a2bb7dc8d57b822295c8f3f1070
Joel Denning <joeldenning@gmail.com>
2020-03-03 10:37:25 -0700

    Minifying with terser (#4)
    * Adding terser plugin. Brings bundle size down from 353kb to 220kb
    
    * Adding source map

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import globals from "rollup-plugin-node-globals";
 import builtins from "rollup-plugin-node-builtins";
+import { terser } from "rollup-plugin-terser";
 
 function createConfig(format) {
   const dir = format === "module" ? "esm" : format;
@@ -9,13 +10,15 @@ function createConfig(format) {
     input: require.resolve("oidc-client/index.js"),
     output: {
       file: `${dir}/index.js`,
+      sourcemap: true,
       format
     },
     plugins: [
       resolve(),
       builtins(),
       globals(),
-      commonjs({ preferBuiltins: false })
+      commonjs({ preferBuiltins: false }),
+      terser()
     ]
   };
 }
